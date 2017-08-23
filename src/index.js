96e8ff9bca75f6256188e8cd5d3cdb5fefcc39f7
Aleksandr Statciuk <free.arhey@gmail.com>
2017-08-23 04:27:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { events } from './bus'
 const defaultPosition = 'bottom'
 
 const prepareBinding = (binding) => {
-  let { expression, modifiers, value } = binding
+  let { modifiers, value } = binding
   let mods = Object.keys(modifiers || {})
 
   return {
-    name: expression,
-    position: mods[0] || defaultPosition,
-    value
+    name: value,
+    position: mods[0] || defaultPosition
   }
 }
 
